2392d44959a9371a13c46c7de97dbb600b843e6d
ppp2037 <ppp2037nn@naver.com>
2020-06-02 19:00:23 +0900

    ddd

---
+++ lib/settings/university_list.dart
@@ -0,0 +1,32 @@
+import '../select_region_page.dart';
+
+List<LeaderBoard> list = <LeaderBoard>[
+  LeaderBoard('가톨릭대학교'),
+  LeaderBoard('건국대학교'),
+  LeaderBoard('경기대학교'),
+  LeaderBoard('경희대학교'),
+  LeaderBoard('고려대학교'),
+  LeaderBoard('광운대학교'),
+  LeaderBoard('국민대학교'),
+  LeaderBoard('동국대학교'),
+  LeaderBoard('명지대학교'),
+  LeaderBoard('삼육대학교'),
+  LeaderBoard('상명대학교'),
+  LeaderBoard('서강대학교'),
+  LeaderBoard('서경대학교'),
+  LeaderBoard('서울대학교 관악캠퍼스'),
+  LeaderBoard('서울대학교 연건캠퍼스'),
+  LeaderBoard('서울과학기술대학교'),
+  LeaderBoard('서울시립대학교'),
+  LeaderBoard('성균관대학교'),
+  LeaderBoard('세종대학교'),
+  LeaderBoard('숭실대학교'),
+  LeaderBoard('연세대학교'),
+  LeaderBoard('중앙대학교'),
+  LeaderBoard('한국외국어대학교'),
+  LeaderBoard('한성대학교'),
+  LeaderBoard('한양대학교'),
+  LeaderBoard('홍익대학교'),
+
+
+];
