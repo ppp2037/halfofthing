370874fbaab5edc2c595b586f23b91e92708f053
ppp2037 <ppp2037nn@naver.com>
2020-06-09 03:27:08 +0900

    채팅페이지 ui 변경, 채팅참여중 아닐 시 텍스트 출력, 스트림빌더 통합, 코드 정리

--- lib/user_chat_page.dart
+++ lib/user_chat_page.dart
@@ -164,146 +164,165 @@ class _User_Chat_PageState extends State<User_Chat_Page> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '반띵을 완료하면 우측상단 완료를 눌러주세요',
-          style: text_grey_15(),
-        ),
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white10,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
-      endDrawer: StreamBuilder<DocumentSnapshot>(
+    return StreamBuilder<DocumentSnapshot>(
         stream: Firestore.instance
             .collection('사용자')
             .document(_userPhoneNumber)
             .snapshots(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) {
+          if (!snapshot.hasData)
             return Center(child: CircularProgressIndicator());
-          } else {
-            Map<String, dynamic> documentFields = snapshot.data.data;
-//            snapshot.data['핸드폰번호']
-            return Drawer(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ListTile(
+          _chattingRoomID = snapshot.data['채팅중인방ID'];
+          chatReference = Firestore.instance
+              .collection("채팅")
+              .document(_chattingRoomID)
+              .collection('messages');
+          return _chattingRoomID == ''
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '현재 진행중인 반띵이 없어요',
+                      style: text_grey_15(),
+                    ),
+                    Container(
+                      height: 60,
+                    ),
+                    Text(
+                      '왼쪽 다른 반띵에 참가하거나',
+                      style: text_grey_15(),
+                    ),
+                    Container(
+                      height: 20,
+                    ),
+                    Text(
+                      '가운데 시작을 눌러 반띵을 시작해보세요',
+                      style: text_grey_15(),
+                    ),
+                  ],
+                )
+              : Scaffold(
+                  appBar: AppBar(
                     title: Text(
-                        '상대방 반띵 횟수 : ' + snapshot.data['이용횟수'].toString(),
-                        style: text_grey_20()),
+                      '반띵을 완료하면 우측상단 완료를 눌러주세요',
+                      style: text_grey_15(),
+                    ),
+                    automaticallyImplyLeading: false,
+                    backgroundColor: Colors.white10,
+                    elevation: 0,
+                    iconTheme: IconThemeData(color: Colors.black),
                   ),
-                  ListTile(
-                    title: Text('반띵 완료하기', style: text_grey_20()),
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => Survey_Page()));
-                    },
+                  endDrawer: Drawer(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(
+                            Icons.account_circle,
+                            color: Colors.grey,
+                          ),
+                          title: Text(
+                              '상대방 반띵 횟수 : ' + snapshot.data['이용횟수'].toString(),
+                              style: text_grey_20()),
+                        ),
+                        Container(
+                          height: 40,
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.restaurant,
+                            color: Colors.grey,
+                          ),
+                          title: Text('반띵 완료하기', style: text_grey_20()),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => Survey_Page()));
+                          },
+                        ),
+                        Container(
+                          height: 40,
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.clear,
+                            color: Colors.grey,
+                          ),
+                          title: Text('다른 반띵하기', style: text_grey_20()),
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => Background_Page()));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  ListTile(
-                    title: Text('다른 반띵하기', style: text_grey_20()),
-                    onTap: () {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => Background_Page()));
-                    },
+                  body: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        StreamBuilder<QuerySnapshot>(
+                          stream: Firestore.instance
+                              .collection("채팅")
+                              .document(snapshot.data['채팅중인방ID'])
+                              .collection('messages')
+                              .orderBy('time', descending: true)
+                              .snapshots(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<QuerySnapshot> snapshot) {
+                            if (!snapshot.hasData)
+                              return Center(child: CircularProgressIndicator());
+                            return Expanded(
+                              child: ListView(
+                                reverse: true,
+                                children: generateMessages(snapshot),
+                              ),
+                            );
+                          },
+                        ),
+                        Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            color: Colors.white,
+                            child: _buildTextComposer()),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            );
-          }
-        },
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-          stream: Firestore.instance
-              .collection('사용자')
-              .document(_userPhoneNumber)
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              _chattingRoomID = snapshot.data['채팅중인방ID'];
-              chatReference = Firestore.instance
-                  .collection("채팅")
-                  .document(_chattingRoomID)
-                  .collection('messages');
-              return Container(
-                color: Colors.yellow.withAlpha(64),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      StreamBuilder<QuerySnapshot>(
-                        stream: chatReference
-                            .orderBy('time', descending: true)
-                            .snapshots(),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<QuerySnapshot> snapshot) {
-                          if (!snapshot.hasData)
-                            return CircularProgressIndicator();
-                          return Expanded(
-                            child: ListView(
-                              reverse: true,
-                              children: generateMessages(snapshot),
-                            ),
-                          );
-                        },
-                      ),
-                      Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          color: Colors.white,
-                          child: _buildTextComposer()),
-                      Builder(builder: (BuildContext context) {
-                        return Container(width: 0.0, height: 0.0);
-                      })
-                    ],
-                  ),
-                ),
-              );
-            }
-          }),
-    );
-  }
-
-  IconButton getDefaultSendButton() {
-    return IconButton(
-      icon: Icon(Icons.send),
-      onPressed: _isWritting ? () => _sendText(_textController.text) : null,
-    );
+                );
+        });
   }
 
   Widget _buildTextComposer() {
-    return IconTheme(
-        data: IconThemeData(
-          color: _isWritting
-              ? Theme.of(context).accentColor
-              : Theme.of(context).disabledColor,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Row(
-            children: <Widget>[
-              Flexible(
-                child: TextField(
-                  controller: _textController,
-                  onChanged: (String messageText) {
-                    setState(() {
-                      _isWritting = messageText.length > 0;
-                    });
-                  },
-                  onSubmitted: _sendText,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                  ),
-                ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              style: text_black_15(),
+              controller: _textController,
+              onChanged: (String messageText) {
+                setState(() {
+                  _isWritting = messageText.length > 0;
+                });
+              },
+              onSubmitted: _sendText,
+              decoration: InputDecoration(
+                border: InputBorder.none,
               ),
-              getDefaultSendButton(),
-            ],
+            ),
           ),
-        ));
+          IconButton(
+            icon: Icon(
+              Icons.send,
+              color: _isWritting ? Colors.pink : Colors.grey,
+            ),
+            onPressed:
+                _isWritting ? () => _sendText(_textController.text) : null,
+          ),
+        ],
+      ),
+    );
   }
 
   Future<Null> _sendText(String text) async {
